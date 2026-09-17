2ae06f4358516e53d3daf3ddcc3fb2c21f1b40ec
Oniefiel <oniefielgarcia12@gmail.com>
2026-09-17 21:14:35 +0800

    Fix tablet responsive login layout

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './responsive.css'
 import AuthGate from './AuthGate.jsx'
 
 createRoot(document.getElementById('root')).render(
